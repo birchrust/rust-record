eac7d8b205a7ef9428958536212a62cc1c827ffb
birch <pickboa@gmail.com>
2023-07-06 20:16:49 +0800

    变量绑定与解构

---
+++ basic/target/debug/basic.d
@@ -0,0 +1,1 @@
+D:\code\rust-study\basic\target\debug\basic.exe: D:\code\rust-study\basic\src\main.rs
---
+++ basic/target/debug/deps/basic-d12f96c11993822f.d
@@ -0,0 +1,5 @@
+D:\code\rust-study\basic\target\debug\deps\basic-d12f96c11993822f.rmeta: src\main.rs
+
+D:\code\rust-study\basic\target\debug\deps\basic-d12f96c11993822f.d: src\main.rs
+
+src\main.rs:
